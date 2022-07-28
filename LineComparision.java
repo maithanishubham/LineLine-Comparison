06a3a408a0dbc5a4a068c9ae3603f62618bdf00e
shubham maithani <shubhammaithani1@gmail.com>
2022-07-28 01:19:30 -0700

    Calculating the length

--- LineComparision.java
+++ LineComparision.java
@@ -4,5 +4,10 @@ public class LineComparision {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Line Comparision");
-	}}
+		int x1= 1, y1= 1, x2= 5, y2= 5;
+		double distance=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		System.out.println("Length of The Line is :" +distance);
+
+		}
+
+	}
